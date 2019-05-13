43d3ced1f69ac1b46c2946aee269680db0306132
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-13 03:55:20 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -38,7 +38,7 @@ def reformat_languages(styles)
   styles.each do |style, languages| 
     languages.each do |language, type|
       new_hash[language] = {:type => type[:type]}
-      if new_hash[language]key?(:style)
+      if new_hash[language].key?(:style)
         new_hash[language][:style].push(style)
       else
         new_hash[language][:style] = [style]
